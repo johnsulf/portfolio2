bc7f8a8484d7ef0ed9544b9c85f6fb088ebc8dde
Erlend Johnsen <erea@ihelse.net>
2025-11-17 14:03:21 +0100

    adds image

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [NgOptimizedImage],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
-
-}
+export class Home {}
