29eafa917ac01fd02adf31b6c42d082c45ce7a6f
Erlend Johnsen <erea@ihelse.net>
2025-11-17 10:37:42 +0100

    adds primeng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,11 +6,18 @@ import {
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
   ],
 };
