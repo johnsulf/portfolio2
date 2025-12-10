aef3105eaf3b1315c056ac2d0c9226ff69baf953
Erlend Johnsen <erea@ihelse.net>
2025-12-10 08:59:37 +0100

    finished cards

--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -9,21 +9,23 @@ export class HomeService {
     {
       title: 'Project Exam 2',
       description:
-        'Holidaze web app – browse venues, create a user to book venues or become a venue manager to create and host your own venues.',
+        'The final exam for my studies at Noroff. Holidaze web app – browse venues, create a user to book venues or become a venue manager to create and host your own venues.',
       screenshot: 'assets/pe2.png',
       githubRepo: 'https://github.com/johnsulf/project-exam-2',
       liveDemo: 'https://johnsulf.github.io/project-exam-2/',
     },
     {
-      title: 'JavaScript Frameworks CA',
-      description: 'This is a simple eCommerce demo built with React, TypeScript and Vite.',
+      title: 'JSF CA',
+      description:
+        'The Course Assignment for the JavaScript Frameworks course. This is a simple eCommerce demo built with React, TypeScript and Vite.',
       screenshot: 'assets/jsfca.png',
       githubRepo: 'https://github.com/johnsulf/jsf-ca-ecom-store',
       liveDemo: 'https://jsf-ca-ecom-store.netlify.app/',
     },
     {
       title: 'Semester Project 2',
-      description: 'Description for Semester Project 2.',
+      description:
+        'The second semester project for my studies at Noroff. A responsive web app for a fictional auction house.',
       screenshot: 'assets/sp2.png',
       githubRepo: 'https://github.com/johnsulf/semester-project-2',
       liveDemo: 'https://johnsulf.github.io/semester-project-2/',
