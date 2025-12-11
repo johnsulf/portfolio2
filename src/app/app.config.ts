8a15b7f9a0eb8ea3bedfcdf11a690ef22a57f931
Erlend Johnsen <erea@ihelse.net>
2025-12-11 09:25:40 +0100

    updates angular and prime

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,14 +8,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
 import { Preset } from '../theme/preset';
-import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideAnimations(),
     providePrimeNG({
       theme: {
         preset: Preset,
